8f42ff6170acc2bf3730fcafe3576d2e46e02471
rodrigo costa <rodrigo.alves@bairesdev.com>
2021-07-27 13:46:50 -0300

    Add project files.

---
+++ Generics/Printer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Printer
+{
+
+	/**
+	*    Name: PrintArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+	public static void PrintArray<T>(T[] array)
+	{
+		foreach (T elem in array)
+		{
+			Console.WriteLine(elem);
+		}
+	}
+
+
+	static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
